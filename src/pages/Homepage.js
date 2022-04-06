abb6d3c56035bf3c70e942a0a5bc317f702fe21e
CHETcica <61766789+CHETcica@users.noreply.github.com>
2022-04-06 14:05:29 +0700

    map

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,34 +1,33 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchAllBook } from "../action"
+import { fetchAllBook } from "../action";
 
 const Homepage = () => {
   const books = useSelector((state) => state.books);
   const disPatch = useDispatch();
   console.log(books);
   useEffect(() => {
-    disPatch(
-        fetchAllBook()
-
-    );
+    disPatch(fetchAllBook());
   }, []);
   return (
-    <div className="container mx-auto grid">
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure>
-          <img
-            src="https://api.lorem.space/image/shoes?w=400&h=225"
-            alt="Shoes"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+    <div className="container mx-auto grid grid-cols-3 gap-4">
+      {books.map((el) => (
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://api.lorem.space/image/shoes?w=400&h=225"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
