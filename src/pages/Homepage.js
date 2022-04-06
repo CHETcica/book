379bf2fc6a47b8d13e05b9d8a97a8deacd999da8
CHETcica <61766789+CHETcica@users.noreply.github.com>
2022-04-06 13:52:09 +0700

    Initial commit

---
+++ src/pages/Homepage.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAllBook } from "../action"
+
+const Homepage = () => {
+  const books = useSelector((state) => state.books);
+  const disPatch = useDispatch();
+  console.log(books);
+  useEffect(() => {
+    disPatch(
+        fetchAllBook()
+
+    );
+  }, []);
+  return (
+    <div className="container mx-auto grid">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="https://api.lorem.space/image/shoes?w=400&h=225"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
